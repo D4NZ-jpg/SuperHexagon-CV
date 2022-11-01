308e1708f2b5cc342d8d2949234b05b566d7312e
D4NZ-jpg <57308957+D4NZ-jpg@users.noreply.github.com>
2022-11-01 09:36:44 -0600

    Improved middle detection

--- SuperHexagon/src/main.cpp
+++ SuperHexagon/src/main.cpp
@@ -6,6 +6,11 @@
 
 constexpr float crop = 0.1;
 
+bool sortContourArea(std::vector<cv::Point> a, std::vector<cv::Point> b)
+{
+	return cv::contourArea(a) > cv::contourArea(b);
+}
+
 cv::Mat captureScreen(HWND hwnd)
 {
 	HDC hwindowDC, hwindowCompatibleDC;
@@ -62,7 +67,6 @@ int main()
 	HWND hwndWindow = FindWindow(NULL, stemp.c_str());
 
 	int key = 0;
-
 	while (key != 27)
 	{
 		//Capture images
@@ -74,52 +78,38 @@ int main()
 		cv::Mat hsvChannels[3];
 		cv::split(hsvImg, hsvChannels);		
 		cv::threshold(hsvChannels[2], grayImg, 120, 255, cv::THRESH_BINARY);
-		
-		//Find middle shape
-		// 
+
 		//Crop middle
 		cv::Rect centerRect(
 			grayImg.size().width / 2 - grayImg.size().width * 0.15,
 			grayImg.size().height / 2 - grayImg.size().height * 0.15,
 			grayImg.size().width * 0.3,
 			grayImg.size().height * 0.3
-			);
+		);
 
-		cv::Mat centerImg = grayImg(centerRect), foundMat = cv::Mat::zeros({centerRect.width, centerRect.height}, CV_8UC1);
-		int maxRad = min(centerImg.size().width, centerImg.size().height);
-		for (int i = 5; i < maxRad / 2; i++)
-		{
-			cv::Mat circleMask = cv::Mat::zeros(centerImg.size(), CV_8UC1);
-			cv::circle(circleMask,{centerImg.size().width / 2 - 1, centerImg.size().height / 2}, i, cv::Scalar(255, 255, 255));
-			centerImg.copyTo(foundMat, circleMask);
-
-			if (cv::countNonZero(foundMat) > 100)
-				break;
-		}
-
-		//Get edges
-		cv::blur(foundMat, foundMat, { 3,3 });
-		cv::Canny(foundMat, foundMat, 125, 125 * 2, 3);
-		cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, { 4,4 });
-		cv::dilate(foundMat, foundMat, kernel);
-
+		cv::Mat centerImg = grayImg(centerRect);
 		std::vector<std::vector<cv::Point>> contours;
-		std::vector<cv::Vec4i> hierarchy;
-		cv::findContours(foundMat, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-		//Draw the edges
-		cv::Mat test = src(centerRect);
-		for (auto& i : contours)
+		cv::findContours(centerImg, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+		
+		if (contours.size() >= 2)
 		{
-			cv::Rect bounds = cv::boundingRect(i);
-			cv::circle(test, {bounds.x + bounds.width/2, bounds.y + bounds.height/2}, 1, {0,255,0});
-			cv::line(src, { centerRect.x + bounds.x + bounds.width / 2, centerRect.y + bounds.y + bounds.height / 2 }, { src.size().width / 2 - 1, src.size().height / 2 }, { 0, 255, 0 });
+			std::vector<cv::Point> approx;
+			std::sort(contours.begin(), contours.end(), sortContourArea);
+
+			float epsilon = 0.04 * cv::arcLength(contours[1], true);
+			cv::approxPolyDP(contours[1], approx, epsilon, true);
+
+			for (auto& p : approx)
+			{
+				p.x += centerRect.x;
+				p.y += centerRect.y;
+			}
+
+			cv::drawContours(src, std::vector<std::vector<cv::Point>>(1, approx), -1, { 0,255,0 }, 4);
 		}
 
 		//Display window
 		cv::imshow("Super Hexagon | CV", src);
-		cv::imshow("Center", test);
-
 
 		key = cv::waitKey(1);
 	}
